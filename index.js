34a57cacf152ea3ad1a5026cb8620a712016fc13
Patsoks <patsoks.sokari@gmail.com>
2018-11-28 10:02:19 +0100

    Add create red-flag incident handler

--- index.js
+++ index.js
@@ -9,6 +9,36 @@ const port = process.env.PORT || 3000;  //setup port
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}));
 
+app.post('/api/v1/red-flags/', (req, res) => {
+	let createdBy = req.body.userId;
+	let date = new Date();
+	let details = req.body.postData;
+
+	const post = {
+		postId: redFlagIncidents.length + 1,
+		createdBy,
+		createdOn: date,
+		comment: details,
+		type: "red-flag",
+		"status": "draft",
+		"location": "",
+		images: [],
+	}
+
+	if(redFlagIncidents.push(post))
+		res.status(201).send({
+			"status": 201,
+			postId,
+			post,
+		});
+	else
+		res.status(500).send({
+			status: 500,
+			"message": "Post could not be create at this time.",
+		});
+
+});
+
 //start the app
 app.listen(port, () => {
     console.log(`Server up on localhost:${port}!`); 
