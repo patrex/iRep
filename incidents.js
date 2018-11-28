ed7ebb190bd5e66fca4ec53ba6c4ef7913220a79
Patsoks <patsoks.sokari@gmail.com>
2018-11-28 21:35:30 +0100

    Modify index.js to improve some existing features(delete and location patch)

--- incidents.js
+++ incidents.js
@@ -1,11 +1,34 @@
-let incidents = [{
-    id: 1,
-    createdOn: new Date(),
-    createdBy: 2,
-    type: "red-flag",
-    location: "23, 34.67",
-    images: [],
-    comments: "this transpired today",
-}]
+let incidents = [
+    {
+        id: 1,
+        createdOn: new Date(),
+        createdBy: 2,
+        type: "red-flag",
+        location: "23, 34.67",
+        images: [],
+        comments: "this transpired today",
+    },
+
+    {
+        id: 2,
+        createdBy: 4,
+        type: "intervention",
+        location: "6.54, 3.36",
+        images: [],
+        comments: "Army molestion",
+        status: "draft",
+    },
+
+    {
+        id: 3,
+        createdOn: new Date(),
+        createdBy: 5,
+        type: "red-flag",
+        location: "23, 34.67",
+        images: [],
+        comments: "this transpired today",
+        status: "resolved",
+    }
+]
 
 export default incidents;
--- index.js
+++ index.js
@@ -10,82 +10,116 @@ const port = process.env.PORT || 3000;  //setup port
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.post('/api/v1/red-flags/', (req, res) => {
+//create a red-flag record		!Done(specialize success message for each type of incident)
+app.post('/api/v1/red-flags', (req, res) => {
 	let createdBy = req.body.userId;
 	let date = new Date();
 	let details = req.body.postData;
+	let type = req.body.type;
+
+	let id = incidents.length + 1;
 
 	const post = {
-		postId: redFlagIncidents.length + 1,
+		id,
 		createdBy,
 		createdOn: date,
 		comment: details,
-		type: "red-flag",
+		type,
 		"status": "draft",
 		"location": "",
 		images: [],
 	}
 
-	if(redFlagIncidents.push(post))
+	if(incidents.push(post))
 		res.status(201).send({
 			"status": 201,
-			postId,
+			id,
 			post,
+			message: "Created red-flag record",
 		});
 	else
 		res.status(500).send({
 			status: 500,
-			"message": "Post could not be create at this time.",
+			"message": "Post could not be created at this time.",
 		});
 
 });
+
+//return all red-flag incidents. DONE!
 app.get('/api/v1/red-flags', (req, res) => {
-	res.json({
-		"status": 200,
-		"data"  : redFlagIncidents,
+	//use map to select only red-flag incidents
+	let redFlagIncidents = incidents.filter( (incident) => incident.type === "red-flag" );
+	
+	if(redFlagIncidents.length > 0){
+		res.json({
+			"status": 200,
+			"data"  : redFlagIncidents,
+		});
+	} 
+
+	res.status(404).json({
+		"status": 404,
+		"message": "No red-flag incidents found",
 	});
 })
-app.get('/api/v1/red-flags/:red-flag-id', (req, res) => {
-	const redFlagId = parseInt(req.params.red-flag-id, 10);
-	redFlagIncidents.map(redIncident) => {
+
+//get a specific red-flag incident. DONE!
+app.get('/api/v1/red-flags/:redFlagID', (req, res) => {
+	const redFlagId = parseInt(req.params.redFlagID, 10);
+
+	incidents.map( (redIncident) => {
 		if(redIncident.id == redFlagId){
 			res.send({
 				"status": 200,
 				redIncident,
 			});
 		}
-	}
+	});
 
 	res.send({
 		"status": 404,
 		message: "we could not find a red-flag incident with that id",
-app.patch('/red-flags/redID/location', (req, res) => {
-	const id = parseInt(req.params.redID, 10);
+	});
+});
 
-	redFlagIncidents.map(redEvent) => {
-		if(redFlagIncidents.id == id){
-			redEvent.location
+//add a location for a specific red-flag incident
+app.patch('/api/v1/red-flags/:redFlagID/:location', (req, res) => {
+	const rID = parseInt(req.params.redFlagID, 10);
+	const location = req.params.location;
+
+	incidents.map((redEvent, position) => {
+		if(redEvent.id == rID){
+			redEvent.location = location;
+			//incidents.splice(position, 1);	//!
+			res.send({
+				status: 201,
+				message: `location for red-flag incident with id [ ${rID} ] was successfully updated.`
+			});
 		}
 	});
+});
+
+//delete a particular red-flag incidents !DONE
 app.delete('/api/v1/red-flags/:redFlagID', (req, res) => {
 	const id = parseInt(req.params.redFlagID, 10);
 
-	redFlagEvents.map((redEvent, position) => {
+	incidents.map((redEvent, position) => {
 		if(redEvent.id == id){
-			redFlagIncidents.splice(position, 1);
+			incidents.splice(position, 1);	//!
 			res.send({
 				status: 201,
-				message: `red-flag incident with id: ${id} was successfully deleted`
+				message: `red-flag incident with id [ ${id} ] was successfully deleted`
 			});
 		}
-	)}
-		res.send({
-			status: 404,
-			message: "Sorry, it appears that there's no event with your id on the database",
-		});
+	});
+
+	res.send({
+		status: 404,
+		message: "Sorry, it appears that there's no red-flag record with your id",
+	});
 });
 
 //start the app
 app.listen(port, () => {
-    console.log(`Server up on localhost:${port}!`); 
+    console.log(`Server up on localhost:${port}`); 
 });
