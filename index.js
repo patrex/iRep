7dc6cd94b3a482cf6212fbfd0ab658a67b9e85b5
Patsoks <patsoks.sokari@gmail.com>
2018-11-28 08:41:56 +0100

    Add handler to get all red-flag incidents

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 import express from 'express';
 import users from './users';
 import incidents from './incidents';
@@ -8,6 +9,13 @@ const port = process.env.PORT || 3000;  //setup port
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/api/v1/red-flags', (req, res) => {
+	res.json({
+		"status": 200,
+		"data"  : redFlagIncidents,
+	});
+})
 
 //start the app
 app.listen(port, () => {
