2fdaf243dc392de11cf6f3a578cd677b73d62357
Patsoks <patsoks.sokari@gmail.com>
2018-11-28 10:47:21 +0100

    Add delete red-flag handler

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 import express from 'express';
 import users from './users';
 import incidents from './incidents';
@@ -8,6 +9,24 @@ const port = process.env.PORT || 3000;  //setup port
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}));
+
+app.delete('/api/v1/red-flags/:redFlagID', (req, res) => {
+	const id = parseInt(req.params.redFlagID, 10);
+
+	redFlagEvents.map((redEvent, position) => {
+		if(redEvent.id == id){
+			redFlagIncidents.splice(position, 1);
+			res.send({
+				status: 201,
+				message: `red-flag incident with id: ${id} was successfully deleted`
+			});
+		}
+	)}
+		res.send({
+			status: 404,
+			message: "Sorry, it appears that there's no event with your id on the database",
+		});
+});
 
 //start the app
 app.listen(port, () => {
