680d18f4afc3e087e6016e488e11c283a21fd519
Patsoks <patsoks.sokari@gmail.com>
2018-11-28 09:21:40 +0100

    Add handler to get specific red-flag incident

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 import express from 'express';
 import users from './users';
 import incidents from './incidents';
@@ -8,6 +9,23 @@ const port = process.env.PORT || 3000;  //setup port
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/api/v1/red-flags/:red-flag-id', (req, res) => {
+	const redFlagId = parseInt(req.params.red-flag-id, 10);
+	redFlagIncidents.map(redIncident) => {
+		if(redIncident.id == redFlagId){
+			res.send({
+				"status": 200,
+				redIncident,
+			});
+		}
+	}
+
+	res.send({
+		"status": 404,
+		message: "we could not find a red-flag incident with that id",
+	});
+});
 
 //start the app
 app.listen(port, () => {
