e51584081ba35c979abd6f18e8787aea65171af8
Patsoks <patsoks.sokari@gmail.com>
2018-11-28 11:23:16 +0100

    Add patch handler for location

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ const port = process.env.PORT || 3000;  //setup port
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}));
 
+app.patch('/red-flags/redID/location', (req, res) => {
+	const id = parseInt(req.params.redID, 10);
+
+	redFlagIncidents.map(redEvent) => {
+		if(redFlagIncidents.id == id){
+			redEvent.location
+		}
+	});
+});
+
 //start the app
 app.listen(port, () => {
     console.log(`Server up on localhost:${port}!`); 
