297f7e2568f4596409c977dcfdcadb779f56a036
Patsoks <patsoks.sokari@gmail.com>
2018-11-30 18:33:13 +0100

    Add test dir

---
+++ spec/tests.js
@@ -0,0 +1,20 @@
+var request = require('request');
+const baseURL = 'http://localhost:3000/';
+
+describe("Test End-points", function(){
+    describe("GET /", function(){
+        it("should return a status code of 200 ok", function(done){
+            request.get(baseURL, function(err, res, body){
+                expect(res.statusCode).toBe(200);
+                done();
+            });
+        }); //end it
+
+        it("should have a body with 'data' key", function(done){
+            request.get(baseURL, function(err, res, body){
+                expect(body).toBe('Hello, world');
+                done();
+            });
+        }); //end it
+    }); //end describe
+}); //end describe
