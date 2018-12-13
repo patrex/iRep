ba87f2ab92a415ce259fa1e5518a924d236b0566
Patsoks <patsoks.sokari@gmail.com>
2018-12-13 19:45:44 +0100

    feat(UI): front end features

---
+++ frontend/createUser.js
@@ -0,0 +1,9 @@
+function makeUser(){
+    const name = document.getElementById().nodeValue;
+    const lname = document.getElementById().nodeValue;
+    const onames = document.getElementById().nodeValue;
+    const phone = document.getElementById().nodeValue;
+    const password = document.getElementById().nodeValue;
+
+    // TODO: make an object and call the API to create user
+}
---
+++ frontend/signInLocal.js
@@ -0,0 +1,7 @@
+function signInLocal(){
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('pwd').value;
+
+    //  make object and request sign in
+}
