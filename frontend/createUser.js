2078ee20c9c69b2d270f2e559218a3275f97fd65
Patsoks <patsoks.sokari@gmail.com>
2018-12-14 21:29:57 +0100

    chore(frontend): refactor js files

--- frontend/createUser.js
+++
@@ -1,9 +0,0 @@
-function makeUser(){
-    const name = document.getElementById().nodeValue;
-    const lname = document.getElementById().nodeValue;
-    const onames = document.getElementById().nodeValue;
-    const phone = document.getElementById().nodeValue;
-    const password = document.getElementById().nodeValue;
-
-    // TODO: make an object and call the API to create user
-}
--- frontend/signInLocal.js
+++
@@ -1,7 +0,0 @@
-function signInLocal(){
-
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('pwd').value;
-
-    //  make object and request sign in
-}
