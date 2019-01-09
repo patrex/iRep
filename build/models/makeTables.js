9c698ee88d677ca8810e35d2ad5754118cdd35fb
Patsoks Patsokari <33361294+patrex@users.noreply.github.com>
2019-01-09 11:45:46 +0100

    set ssl

--- build/models/makeTables.js
+++ build/models/makeTables.js
@@ -5,7 +5,8 @@ var _pg = require('pg');
 var connectionString = process.env.DATABASE_URL;
 
 var client = new _pg.Client({
-  connectionString: connectionString
+  connectionString: connectionString,
+  ssl: true,
 });
 
 var tables = 'CREATE TABLE IF NOT EXISTS\n    users(\n      id SERIAL PRIMARY KEY,\n      firstname VARCHAR(128) NOT NULL,\n      lastname VARCHAR(128) NOT NULL,\n      othernames VARCHAR(128) NOT NULL,\n      email VARCHAR(128) NOT NULL UNIQUE,\n      phone VARCHAR(128) NOT NULL UNIQUE,\n      username VARCHAR(128) NOT NULL UNIQUE,\n      password VARCHAR(256) NOT NULL,\n      registered timestamptz NOT NULL,\n      is_admin boolean\n    );\n\nCREATE TYPE status AS ENUM(\'rejected\', \'under-investigation\', \'resolved\');\n    \nCREATE TABLE IF NOT EXISTS\n  images(\n    id SERIAL,\n    owner INT NOT NULL,\n    title varchar(128),\n    uploaded_on timestamptz NOT NULL,\n    foreign key (owner) references users(id),\n    primary key(owner, title)\n);\n\nCREATE TABLE IF NOT EXISTS\n  interventions(\n    id SERIAL PRIMARY KEY,\n    creadted_on timestamptz NOT NULL,\n    created_by VARCHAR(128) NOT NULL,\n    location point,\n    current_status status,\n    comment text,\n    foreign key (created_by) references users(username)\n  );\n\nCREATE TABLE IF NOT EXISTS\n  redflags(\n    id SERIAL PRIMARY KEY,\n    creadted_on timestamptz NOT NULL,\n    created_by VARCHAR(128) NOT NULL,\n    location point,\n    current_status status,\n    comment text,\n    foreign key (created_by) references users(username)\n  );';
