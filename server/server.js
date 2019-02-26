c71271fbef1addb24c0fcfe49369c25a8eb68d2a
Patsoks <patsoks.sokari@gmail.com>
2019-02-26 16:23:13 +0100

    update server.js and gitignore

--- server/server.js
+++ server/server.js
@@ -1,25 +1,6 @@
-import app from '../server/app';
-//import https from 'https';
-import http from 'http';
-//mport fs from 'fs';
+import router from '../server/app';
 
+const port = process.env.PORT || 3000;
+const server = router.listen( port, () => console.log("Server running on localhost:" + port) );
 
-
-const httpport = process.env.PORT || 3000;
-//const httpsport = process.env.PORT || 8443;
-// let key = fs.readFileSync(__dirname, '../selfsigned.key');
-// let cert = fs.readFileSync(__dirname, '../selfsigned.crt');
-
-// let credentials = {
-//     key,
-//     cert
-// }
-
-let httpServer = http.createServer(app);
-//let httpsServer = https.createServer(credentials, app);
-
-
-httpServer.listen( httpport, () => console.log("Server running on localhost:" + httpport) );
-//httpsServer.listen( httpsport, () => console.log("HTTPS Server running on localhost:" + httpsport) );
-
-export default httpServer;
+export default server;
